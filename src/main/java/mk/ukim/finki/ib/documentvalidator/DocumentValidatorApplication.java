5930ff0a0be66038c140f6a959e93d5b60e25255
Boris Djotov <borisdzotov@gmail.com>
2024-09-06 14:41:54 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/ib/documentvalidator/DocumentValidatorApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ib.documentvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/ib/documentvalidator/DocumentValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.ib.documentvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
